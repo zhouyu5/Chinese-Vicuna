ddbd8ae763cec1bbec224df4f96004629323ba28
zhouyu5 <yu.zhou@intel.com>
2023-04-21 13:59:29 +0000

    demo

--- demo.sh
+++ demo.sh
@@ -3,16 +3,15 @@ set -x
 # BASE_MODEL="/home/vmagent/app/data/gpt-neo-2.7B"
 # LORA_PATH="/home/vmagent/app/data/zy_models/lora-gpt-neo-2.7B-alpaca"
 
-# BASE_MODEL="/home/vmagent/app/data/llama-7b-hf,/home/vmagent/app/data/vicuna-7b"
-# LORA_PATH="/home/vmagent/app/data/Chinese-Vicuna-lora-7b-belle-and-guanaco,"
+BASE_MODEL="/home/vmagent/app/data/llama-7b-hf,/home/vmagent/app/data/vicuna-7b"
+LORA_PATH="/home/vmagent/app/data/Chinese-Vicuna-lora-7b-belle-and-guanaco,"
+python app.py --model_path_list $BASE_MODEL --lora_path_list $LORA_PATH \
+    --load_8bit "1,0"
 
-# python app.py --model_path_list $BASE_MODEL --lora_path_list $LORA_PATH \
-#     --load_8bit True
 
-
-BASE_MODEL="/home/vmagent/app/data/vicuna-7b"
-python app.py --model_path_list $BASE_MODEL \
-    --load_8bit "0"
+# BASE_MODEL="/home/vmagent/app/data/vicuna-7b"
+# python app.py --model_path_list $BASE_MODEL \
+#     --load_8bit "0"
 
 
 
